14d81b4b636916f77b12fc8b424c45646a18e169
Daniel <77298801+DWM7721@users.noreply.github.com>
2021-10-08 18:15:33 -0400

    Created Splash Screen

---
+++ AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package ca.kainotomia.it.aphrodite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                doNextWork();
+            }
+        }, 3000);
+
+
+    }
+
+    private void doNextWork() {
+        finish();
+    }
+
+}
