dc3b798fed721356214235c9add2f14cd37a4dfc
alyssaGomezn01042777 <n01042777@humbermail.ca>
2021-11-11 23:42:32 -0500

    Updated Choose Module Location page

--- AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/create_layout/ChooseLayoutLocFragment.java
+++ AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/create_layout/ChooseLayoutLocFragment.java
@@ -14,24 +14,24 @@ import android.view.ViewGroup;
 
 import ca.kainotomia.it.aphrodite.R;
 
-public class ShowSavedLayoutFragment extends Fragment {
+public class ChooseLayoutLocFragment extends Fragment {
 
-    private ShowSavedLayoutViewModel mViewModel;
+    private ChooseLayoutLocViewModel mViewModel;
 
-    public static ShowSavedLayoutFragment newInstance() {
-        return new ShowSavedLayoutFragment();
+    public static ChooseLayoutLocFragment newInstance() {
+        return new ChooseLayoutLocFragment();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.show_saved_layout_fragment, container, false);
+        return inflater.inflate(R.layout.choose_layout_loc_fragment, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ShowSavedLayoutViewModel.class);
+        mViewModel = new ViewModelProvider(this).get(ChooseLayoutLocViewModel.class);
         // TODO: Use the ViewModel
     }
 
--- AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/create_layout/ChooseLayoutLocViewModel.java
+++ AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/create_layout/ChooseLayoutLocViewModel.java
@@ -2,6 +2,6 @@ package ca.kainotomia.it.aphrodite.ui.create_layout;
 
 import androidx.lifecycle.ViewModel;
 
-public class ShowSavedLayoutViewModel extends ViewModel {
+public class ChooseLayoutLocViewModel extends ViewModel {
     // TODO: Implement the ViewModel
 }
