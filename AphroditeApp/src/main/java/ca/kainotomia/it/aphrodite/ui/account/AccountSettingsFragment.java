dfb9c3606c09d5cfb15a40e1458a02f6ff9a7ded
Jose Teodoro <joseateodoro.jt@gmail.com>
2021-11-14 00:33:26 -0500

    UPDATE

--- AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/account/AccountSettingsFragment.java
+++ AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/account/AccountSettingsFragment.java
@@ -15,7 +15,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import ca.kainotomia.it.aphrodite.R;
 
-public class AccountSettingsFragment extends Fragment{
+public class AccountSettingsFragment extends Fragment implements View.OnClickListener{
 
     private AccountViewModel accountViewModel;
     private ViewGroup container;
@@ -29,8 +29,10 @@ public class AccountSettingsFragment extends Fragment{
         View rootView = inflater.inflate(R.layout.fragment_account_settings, container, false);
 
         Button AFSS_Button_terminate = (Button) rootView.findViewById(R.id.AFSS_Button_terminate);
+        Button AFSS_Button_logout = (Button) rootView.findViewById(R.id.AFSS_Button_logout);
 
-        //AFSS_Button_terminate.setOnClickListener(this);
+        AFSS_Button_terminate.setOnClickListener(this);
+        AFSS_Button_logout.setOnClickListener(this);
 
         this.container = container;
         this.inflater = inflater;
@@ -38,22 +40,25 @@ public class AccountSettingsFragment extends Fragment{
         return rootView;
     }
 
+    @Override
+    public void onClick(View view) {
+        Fragment fragment = null;
+        switch (view.getId()) {
+            case R.id.AFSS_Button_terminate:
+                fragment = new Fragment();
+                replaceFragment(fragment);
+                break;
+            case R.id.AFSS_Button_logout:
+                break;
 
-//    public void onClick(View view) {
-//        Fragment fragment = null;
-//        switch (view.getId()) {
-//            case R.id.AFSS_Button_terminate:
-//                fragment = new HomeFragment();
-//                replaceFragment(fragment);
-//                break;
-//
-//        }
-//
-//        public void replaceFragment(Fragment someFragment){
-//            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-//            transaction.replace(R.id.nav_host_fragment, someFragment);
-//            transaction.addToBackStack(null);
-//            transaction.commit();
-//        }
-//    }
+        }
+        public void replaceFragment(Fragment someFragment) {
+            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+            transaction.replace(R.id.nav_host_fragment, someFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+
+    }
+
 }
