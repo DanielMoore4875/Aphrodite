f245a55f329a1d58564e3881c1e05231b5a72a86
Daniel <77298801+DWM7721@users.noreply.github.com>
2021-12-04 02:38:44 -0500

    Create layout fragment will update from database when coming from edit layout page. Cleaning up pages

--- AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/LoginActivity.java
+++ AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/LoginActivity.java
@@ -1,30 +1,21 @@
 package ca.kainotomia.it.aphrodite;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.ContactsContract;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
--- AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/layouts/ViewHolder.java
+++ AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/layouts/ViewHolder.java
@@ -11,52 +11,15 @@ import ca.kainotomia.it.aphrodite.R;
 
 public class ViewHolder extends RecyclerView.ViewHolder {
 
-//    private TextView title;
-//    private ImageView img;
+    //Refactored and removed old code
     private Button btn;
-private GridLayout gridLayout;
-
-//    public LinearLayout root;
-//    public TextView txtTitle;
-//    public TextView txtDesc;
-
 
     public ViewHolder(@NonNull View itemView) {
         super(itemView);
-//        this.title = itemView.findViewById(R.id.FLRVI_textView);
-//        this.img = itemView.findViewById(R.id.FLRVI_imageView);
         this.btn = itemView.findViewById(R.id.FLRVI_btn);
-        this.gridLayout = itemView.findViewById(R.id.temp_GRIDLAY);
-
-//        root = itemView.findViewById(R.id.root);
-//        txtTitle = itemView.findViewById(R.id.list_title);
-//        txtDesc = itemView.findViewById(R.id.list_desc);
-
     }
-
-//    public TextView getTitle() {
-//        return this.title;
-//    }
-//
-//    public ImageView getImg() {
-//        return this.img;
-//    }
-
-
-    public GridLayout getGridLayout() {
-        return gridLayout;
-    }
-
     public Button getBtn() {
         return this.btn;
     }
 
-//    public void setTxtTitle(String string) {
-//        txtTitle.setText(string);
-//    }
-//
-//
-//    public void setTxtDesc(String string) {
-//        txtDesc.setText(string);
-//    }
 }
