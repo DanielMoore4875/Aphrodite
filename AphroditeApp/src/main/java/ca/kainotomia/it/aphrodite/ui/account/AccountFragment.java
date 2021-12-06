d7e22cd2b51087cdf33eb38cd326c1094739e62c
alyssaGomezn01042777 <n01042777@humbermail.ca>
2021-12-05 19:34:53 -0500

    moved logout button into the settings page

--- AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/account/AccountFragment.java
+++ AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/account/AccountFragment.java
@@ -34,7 +34,8 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
         Button AF_Button_about = view.findViewById(R.id.AF_Button_about);
         Button AF_Button_support = view.findViewById(R.id.AF_Button_support);
         Button AF_Button_settings = view.findViewById(R.id.AF_Button_settings);
-        Button AF_Button_changePass = view.findViewById(R.id.AF_Button_changePass);
+        //Button AF_Button_changePass = view.findViewById(R.id.AF_Button_changePass);
+
 
         //get current username and display it in the username textview
         TextView usernameText = view.findViewById(R.id.AF_TextView_username);
@@ -44,13 +45,22 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
         AF_Button_about.setOnClickListener(this);
         AF_Button_settings.setOnClickListener(this);
         AF_Button_support.setOnClickListener(this);
-        AF_Button_changePass.setOnClickListener(this);
+        //AF_Button_changePass.setOnClickListener(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_account, container, false);
+        Button logoutAccount = rootView.findViewById(R.id.AFSS_Button_logout);
+
+        logoutAccount.setOnClickListener(v -> {
+            System.out.println("LOGOUT");
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+        });
 
         return rootView;
     }
@@ -70,10 +80,6 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.AF_Button_settings:
                 fragment = new AccountSettingsFragment();
-                replaceFragment(fragment);
-                break;
-            case R.id.AF_Button_changePass:
-                fragment = new SettingsChangePassFragment();
                 replaceFragment(fragment);
                 break;
         }
--- AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/account/AccountSettingsFragment.java
+++ AphroditeApp/src/main/java/ca/kainotomia/it/aphrodite/ui/account/AccountSettingsFragment.java
@@ -29,12 +29,13 @@ public class AccountSettingsFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_account_settings, container, false);
 
         Button terminateAccount = view.findViewById(R.id.AFSS_Button_terminate);
-        Button logoutAccount = view.findViewById(R.id.AFSS_Button_logout);
+        //Button logoutAccount = view.findViewById(R.id.AFSS_Button_logout);
 
         terminateAccount.setOnClickListener(v -> {
             System.out.println("TERMINATE");
         });
 
+        /*
         logoutAccount.setOnClickListener(v -> {
             System.out.println("LOGOUT");
             FirebaseAuth.getInstance().signOut();
@@ -42,7 +43,7 @@ public class AccountSettingsFragment extends Fragment{
             startActivity(intent);
             getActivity().finish();
         });
-
+        */
 
 
         return view;
