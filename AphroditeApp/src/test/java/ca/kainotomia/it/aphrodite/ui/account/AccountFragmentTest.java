80969c47d3e959b84588e26cd2995e500519836e
Jose Teodoro <joseateodoro.jt@gmail.com>
2021-12-06 16:37:40 -0500

    UPDATE build.gradle (work in progress)
    ADDED AccountFragmentTest.java and SignUpFragmentTest.java

---
+++ AphroditeApp/src/test/java/ca/kainotomia/it/aphrodite/ui/account/AccountFragmentTest.java
@@ -0,0 +1,26 @@
+package ca.kainotomia.it.aphrodite.ui.account;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountFragmentTest {
+
+    @Test
+    public void onViewCreated() {
+
+    }
+
+    @Test
+    public void onCreateView() {
+    }
+
+    @Test
+    public void onClick() {
+
+    }
+
+    @Test
+    public void replaceFragment() {
+    }
+}
---
+++ AphroditeApp/src/test/java/ca/kainotomia/it/aphrodite/ui/login/SignUpFragmentTest.java
@@ -0,0 +1,24 @@
+package ca.kainotomia.it.aphrodite.ui.login;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SignUpFragmentTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onCreateView() {
+    }
+
+    @Test
+    public void createUser() {
+    }
+
+    @Test
+    public void setPBInvisibleAndEnableButtons() {
+    }
+}
